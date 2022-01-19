fedc46f5bfe65e3c6391e418890e94acd7e20455
Abdelmageed23 <abd.abdelmageed@gmail.com>
2022-01-19 18:05:08 +0200

    Initializing APP layer

---
+++ 04-APP/APP_Private.h
@@ -0,0 +1,11 @@
+/*********************************************************/
+/* Author  : Abdelrahman Mohamed              			 */
+/* Date     : 19 Jan	2022                   			 */
+/* Version  : V01                            			 */
+/*********************************************************/
+
+#ifndef APP_PRIVATE_H
+#define APP_PRIVATE_H
+
+
+#endif
