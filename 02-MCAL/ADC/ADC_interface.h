a6ee10a7e5b37b3cf3085a76da2d28ff4d7182f5
M0oAlaa <96453780+M0oAlaa@users.noreply.github.com>
2022-01-19 18:48:43 +0200

    Add files via upload

---
+++ 02-MCAL/ADC/ADC_interface.h
@@ -0,0 +1,10 @@
+#ifndef _ADC_INTERFACE_H_
+#define _ADC_INTERFACE_H_
+
+void ADC_init(void);
+
+uint8_t ADC_u8StartConversionSynch(uint8_t ADC_Channel,uint8_t* u8_Reading);
+
+uint8_t ADC_u8StartConversionASynch(uint8_t ADC_Channel,uint8_t* u8_Reading,void(*NotificFunc)(void));
+
+#endif
