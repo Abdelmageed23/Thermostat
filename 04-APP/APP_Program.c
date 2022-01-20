82b61465aba7cc306013f50d67251cd997b0c19e
Abdelmageed23 <53024894+Abdelmageed23@users.noreply.github.com>
2022-01-20 02:05:08 +0200

    pin 9 and temp sensor need testing

--- 04-APP/APP_Program.c
+++ 04-APP/APP_Program.c
@@ -14,6 +14,7 @@
 #include "../03-HAL/LCD/LCD.h"
 #include "../03-HAL/motor/Motor.h"
 #include "../03-HAL/KEYPAD/KEYPAD_interface.h"
+#include "../03-HAL/TEMP/TEMP_interface.h"
 
 #include "APP_Interface.h"
 #include "APP_Private.h"
@@ -34,8 +35,9 @@ void APP_init()
 void APP_Prog()
 {
 	uint8_t state = Home;
+	volatile uint8_t Actual_Temp = 0;
 	volatile uint8_t key =255;
-	uint8_t value=0;
+	uint8_t desired_temp=0;
 	while(1)
 	{
 	switch (state)
@@ -64,11 +66,7 @@ void APP_Prog()
 
 			break;
 		case SET:
-
-			MOTOR_direction(MOTOR_1,FWD);
-			MOTOR_speed(60);
-
-			value=0;
+			desired_temp=0;
 			key =255;
 			LCD_vidClear();
 			LCD_vidSetPosition(0,0);
@@ -86,41 +84,58 @@ void APP_Prog()
 			if(key != 255)
 			{
 
-				value = value *10+key;
-			}
 
+				LCD_vidWriteNumber(key);
+			}
+			desired_temp = desired_temp *10+key;
 
 		}
-			LCD_vidWriteNumber(value);
-			if(value<17)
+
+			if(desired_temp<Actual_Temp)
 			{
 				MOTOR_off(MOTOR_1);
 			}
-			else if (value> 17 && value <21)
+			else if ((desired_temp-Actual_Temp)<5)
 			{
 				MOTOR_speed(30);
 				MOTOR_direction(MOTOR_1,FWD);
 
 
 			}
-			else if ( value >21 && value<23)
+			else if ( ((desired_temp-Actual_Temp)>7 ) && ((desired_temp-Actual_Temp)<10))
 			{
 				MOTOR_direction(MOTOR_1,FWD);
 				MOTOR_speed(60);
 
 
 			}
-			else if ( value >25)
+			else if ( (desired_temp-Actual_Temp) >13 )
 			{
 				MOTOR_speed(90);
 				MOTOR_direction(MOTOR_1,FWD);
 
 			}
 
-			state =GET;
+			key =KPD_u8GetPressedKey();
+			if(key == 8)	// NEED MODIFICATION TO HOME
+			{
+				state = Home;
+			}
 			break;
 		case GET:
-			LCD_vidWriteNumber(value);
+		//	LCD_vidClear();
+			LCD_vidSetPosition(0,0);
+			LCD_vidWriteString("Actual Temp:");
+		//	LCD_vidSetPosition(1,5);
+		//	LCD_vidWriteString("     ");
+			LCD_vidSetPosition(1,12);
+			Actual_Temp = TEMP_u8GetReading(0);// 0 = ADC channel   ** NEEDED
+			LCD_vidWriteNumber(Actual_Temp);
+			key =KPD_u8GetPressedKey();
+			if(key == 8)	// NEED MODIFICATION TO HOME
+			{
+				state = Home;
+			}
 			break;
 		case ERROR:
 			break;
