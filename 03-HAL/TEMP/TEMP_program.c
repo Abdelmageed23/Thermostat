051f611653f740969930cae3721bea8d2f975afa
M0oAlaa <96453780+M0oAlaa@users.noreply.github.com>
2022-01-20 17:32:35 +0200

    Add files via upload

--- 03-HAL/TEMP/TEMP_program.c
+++ 03-HAL/TEMP/TEMP_program.c
@@ -6,13 +6,18 @@
 #include "TEMP_config.h"
 #include "TEMP_interface.h"
 
+void TEMP_init(void)
+{
+	ADC_init();
+}
+
 uint8_t TEMP_u8GetReading(uint8_t ADC_Channel)
 {
 	uint8_t local_u8TEMP;
-	uint16_t local_u16MiliVolt;
+	unint16_t local_u16MiliVolt;
 	uint8_t local_u8ADC_Read;
 	ADC_u8StartConversionSynch(ADC_Channel,&local_u8ADC_Read);
-	local_u16MiliVolt=(uint16_t)(((uint32_t)local_u8ADC_Read*Vref)/TWO_P_RESOLUTION);
+	local_u16MiliVolt=(unint16_t)(((uint32_t)local_u8ADC_Read*Vref)/TWO_P_RESOLUTION);
 	local_u8TEMP=local_u16MiliVolt/10;
 	return local_u8TEMP;
 
